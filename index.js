10b7737b5ad8d9f29b4e4c1080ffd753db3dc6c9
layan2k <lesliedeeshumba@gmail.com>
2022-01-11 19:56:19 +0200

    update

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const app = express();
+const app = new express();
+const path = require('path');
+const ejs = require('ejs');
+const port = 4000;
 
-app.use('/', (req, res)=>{
-    res.send("Hello World")
-    console.log(req.url)
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+app.get('/about', (req,res)=>{
+    res.render('about')
+})
+app.get('/contact',(req,res)=>{
+    res.render('contact')
+})
+app.get('/post',(req,res)=>{
+    res.render('post')
 })
 
-app.listen(4000)
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+})
