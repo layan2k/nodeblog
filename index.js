f3c411fcdc4c3774690304ba054f40bbbe149b10
layan2k <lesliedeeshumba@gmail.com>
2022-01-16 21:03:40 +0200

    DB connection

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ const app = new express();
 const path = require('path');
 const ejs = require('ejs');
 const port = 4000;
+const mongoose = require('mongoose');
+require("dotenv/config");
+const bodyParser = require('body-parser');
 
+
+// models
+const BlogPost = require('./models/BlogPost')
+
+dbkey = process.env.DB_KEY
+
+// mongodb connection
+mongoose.connect('mongodb://127.0.0.1:27017/project1').then(()=>{console.log("db connected")}).catch((err)=>{console.log(err)})
+
+// Express Settings
 app.use(express.static(__dirname + '/public'))
-
 app.set('view engine','ejs');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
-app.get('/',(req,res)=>{
-    res.render('index')
+// routes
+app.get('/',async (req,res)=>{
+    const blogposts = await BlogPost.find({})
+    res.render('index',{blogposts})
 })
 app.get('/about', (req,res)=>{
     res.render('about')
@@ -17,9 +33,27 @@ app.get('/about', (req,res)=>{
 app.get('/contact',(req,res)=>{
     res.render('contact')
 })
-app.get('/post',(req,res)=>{
-    res.render('post')
+
+app.get('/post/new', (req,res)=>{
+    res.render('create')
 })
+
+app.get('/post/:id',async(req,res)=>{
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render('post', {blogpost})
+})
+
+app.post('/posts/store',(req,res)=>{
+    let image = req.files.image
+    image.mv(path.resolve(__dirname,'public/assets/img',image.name),async(error)=>{
+        await BlogPost.create({
+            ...req.body,
+            image: '/img/' + image.name
+        })
+        res.redirect('/')
+    })
+
+} )
 
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`)
